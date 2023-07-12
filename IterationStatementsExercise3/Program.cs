32f7467aef0131d78ec81d83304d9cdce5619159
Nikolay Loboda <nick12137@gmail.com>
2023-07-11 21:14:51 -0500

    Fixed issues

--- IterationStatementsExercise3/Program.cs
+++ IterationStatementsExercise3/Program.cs
@@ -8,66 +8,114 @@ namespace IterationStatements
 
 
         //Call the methods to test them in the Main method below
+
         static void Main(string[] args)
         {
             PrintThousand();
-        }
+            PauseConsole();
 
-            //Write a method that will print to the console all numbers 1000 through - 1000
-
-            public static void PrintThousand()
-            {
-              
-                for (int i = 1000; i >= -1000; i--)
-                {
-                    Console.WriteLine(i);
-
-                }
-
-            //Write a method that will print to the console numbers 3 through 999 by 3 each time
             ByThrees();
+            PauseConsole();
 
-            static void ByThrees()
+
+
+            int userNum1 = GetUserNumber();
+            int userNum2 = GetUserNumber();
+
+            bool areEqual = EqualChecker(userNum1, userNum2);
+
+            Console.WriteLine($"{userNum1} is equal to {userNum2}: {areEqual}");
+            Console.WriteLine("For Next part Press Enter");
+            PauseConsole();
+
+
+            EvenOrOdd();
+            PauseConsole();
+
+            PositiveOrNegative();
+            PauseConsole();
+
+            voteage();
+            PauseConsole();
+
+            Multiplyfrom1to12();
+            PauseConsole();
+
+
+        }
+        //Write a method that will print to the console all numbers 1000 through - 1000
+
+        private static void PrintThousand()
+        {
+
+            for (int i = 1000; i >= -1000; i--)
             {
+                Console.WriteLine(i);
 
-                for (int k = 3; k <= 999; k += 3)
-                {
-                    Console.WriteLine(k);
-
-                }
-                //Write a method to accept two integers as parameters and check whether they are equal or not
+            }
+            Console.WriteLine("For Next part Press Enter");
+        }
+        //Write a method that will print to the console numbers 3 through 999 by 3 each time
 
 
-                Console.WriteLine(counterNumbers(3, 7));
+        static void ByThrees()
+        {
 
-                static bool counterNumbers(int a, int b)
+            for (int k = 3; k <= 999; k += 3)
+            {
+                Console.WriteLine(k);
 
-                {
-                    var check = (a == b) ? true : false;
-
-                    return check;
-                }
-                //Write a method to check whether a given number is even or odd
-                { 
-                int number;
-                Console.Write("Enter an integer :");
-                number = Int32.Parse(Console.ReadLine());
-                if (number % 2 == 0)
-                {
-                    Console.WriteLine("Your number is even.");
-                }
-                else
-                {
-                    Console.WriteLine("Your number is odd.");
-                }
-                Console.ReadKey();
             }
 
-                //Write a method to check whether a given number is positive or negative
 
-                {
+            Console.WriteLine("For Next part Press Enter");
+        }
+
+        //Write a method to accept two integers as parameters and check whether they are equal or not
+
+        private static int GetUserNumber()
+        {
+            Console.WriteLine("Give me a number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            return userNumber;
+
+        }
+
+        private static bool EqualChecker(int num1, int num2)
+
+        {
+            return num1 == num2;
+
+        }
+        //Write a method to check whether a given number is even or odd
+
+        private static void EvenOrOdd()
+        {
+
+            int number;
+            Console.Write("Enter an integer :");
+            number = Int32.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Your number is even.");
+            }
+            else
+            {
+                Console.WriteLine("Your number is odd.");
+
+            }
+            Console.WriteLine("For Next part Press Enter twice");
+
+            Console.ReadKey();
+
+        }
+
+        //Write a method to check whether a given number is positive or negative
+        private static void PositiveOrNegative()
+         {
                     int num;
-                    Console.Write("Enter a number that is positive or negative :");
+        Console.Write("Enter a number that is positive or negative :");
                     num = Int32.Parse(Console.ReadLine());
                     if (num >=0)
                     {
@@ -77,52 +125,40 @@ namespace IterationStatements
                     {
                         Console.WriteLine("Your number is negative.");
                     }
-                    Console.ReadKey();
+
+            Console.WriteLine("For Next part Press Enter twice");
+            Console.ReadKey();
                 }
 
-                //Write a method to read the age of a candidate and determine whether they can vote.
-                //Hint: Use Parse or the safer TryParse() for an extra challenge
-                //Parse()
-                //TryParse()
+        //Heatin Up Section:
+        //Write a method to read the age of a candidate and determine whether they can vote.
 
-                int Voteage;
-                Console.Write("How old are you? :");
-                Voteage = Int32.Parse(Console.ReadLine());
-                if (Voteage >= 18)
-                {
-                    Console.WriteLine("You are of age to vote.");
-                }
-                else
-                {
-                    Console.WriteLine("You are to young to vote.");
-                }
-                Console.ReadKey();
-
-
-                //Heatin Up Section:
-                //Write a method to check if an integer(from the user) is in the range -10 to 10
-
-                int InBetween;
-                Console.Write("Enter a number :");
-                InBetween = Int32.Parse(Console.ReadLine());
-                if (InBetween >= -10 && InBetween <= 10)
-                {
-                    Console.WriteLine("You are in between -10 and 10.");
-                }
-                else
-                {
-                    Console.WriteLine("You are not in between -10 and 10.");
-                }
-                Console.ReadKey();
+        private static void voteage()
+        {
+            int Voteage;
+            Console.Write("How old are you? :");
+            Voteage = Int32.Parse(Console.ReadLine());
+            if (Voteage >= 18)
+            {
+                Console.WriteLine("You are of age to vote.");
+            }
+            else
+            {
+                Console.WriteLine("You are to young to vote.");
+            }
+            Console.ReadKey();
+            Console.WriteLine("For Next part Press Enter twice");
+        }
 
 
 
-                //Write a method to display the multiplication table(from 1 to 12) of a given integer
+        //Write a method to display the multiplication table(from 1 to 12) of a given integer
 
+        private static void Multiplyfrom1to12()
+        { 
+              int x, n;
 
-                int x, n;
-
-                Console.Write("\n\n");
+        Console.Write("\n\n");
                 Console.Write("Display the multiplication table:\n");
                 Console.Write("-----------------------------------");
                 Console.Write("\n\n");
@@ -130,18 +166,28 @@ namespace IterationStatements
                 Console.Write("Input the number (Table to be calculated) : ");
                 n = Convert.ToInt32(Console.ReadLine());
                 Console.Write("\n");
-                for (x = 1; x <= 12; x++)
-                {
-                    Console.Write("{0} X {1} = {2} \n", n, x, n * x);
-                }
+            for (x = 1; x <= 12; x++)
+            {
+                Console.Write("{0} X {1} = {2} \n", n, x, n * x);
             }
-        }
-
     }
 
-        }
 
-        
-            
-        
+    private static void PauseConsole()
+        {
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
+
+
     
+        
+
+    
+
+
+
+
+
